3da7446afaf60f1de99951737b8dfbe6d7fad6ed
Surya <suryavamsi04@gmail.com>
2023-06-17 17:29:47 +0530

    link

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 function Header() {
   return (
     <div className='header'>
-      <Link to="/C" style={{color:'red', fontSize:'28px'}}><b>CeleMart</b></Link>
+      <Link to="/Celemart" style={{color:'red', fontSize:'28px'}}><b>CeleMart</b></Link>
       <nav>
       <Link to="/cart">Cart</Link>
       <Link to="/">Login</Link>
